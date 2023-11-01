08f8dbb7ed97eda5ad3f6dd3cbc0b19035acb4d7
acodered <alexkondratru@gmail.com>
2023-11-01 20:53:37 +0100

    Add models to admin.py

--- expensetracker/admin.py
+++ expensetracker/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import (Settings,
+                     Account,
+                     Transaction,
+                     Currency,
+                     Category) 
 # Register your models here.
+admin.site.register(Settings)
+admin.site.register(Account)
+admin.site.register(Transaction)
+admin.site.register(Currency)
+admin.site.register(Category)
+
