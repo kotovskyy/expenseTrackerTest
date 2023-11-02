183b7e9c5056717ab6371be0eab6ae12043d052d
acodered <alexkondratru@gmail.com>
2023-11-02 11:27:02 +0100

    Send data to index.html page

--- expensetracker/views.py
+++ expensetracker/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Account
+
 # Create your views here.
 def index(request):
-    return render(request, 'expensetracker/index.html')
+    return render(request, 'expensetracker/index.html', context={
+        "accounts":Account.objects.all()
+    })
