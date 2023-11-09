213290695255240c5162f4875f9ca4698384d239
acodered <alexkondratru@gmail.com>
2023-11-09 17:55:32 +0100

    Remove 'firstapp' test application

--- databasetest/urls.py
+++ databasetest/urls.py
@@ -19,7 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('firstapp/', include('firstapp.urls')),
     path('expensetracker/', include('expensetracker.urls')),
     path('users/', include('users.urls')),
 ]
