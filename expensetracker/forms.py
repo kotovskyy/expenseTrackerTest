e117bc3817294602aaae87d791f6d8b6b31dc594
acodered <alexkondratru@gmail.com>
2023-11-09 15:44:33 +0100

    Change form to use choice fields

--- expensetracker/forms.py
+++ expensetracker/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from datetime import date
-from .models import Account
+from .models import Account, Currency
 
 class AddTransactionForm(forms.Form):
         
@@ -10,12 +10,23 @@ class AddTransactionForm(forms.Form):
         max_digits=22,
         decimal_places=2
     )
-    account = forms.CharField(max_length=100)
+    currency = forms.ChoiceField(
+        choices=[(c.id, c.code) for c in Currency.objects.all()],
+        initial=None
+    )
+    account = forms.ChoiceField(choices=[])
     date = forms.DateField(
         label="Date",
+        initial=date.today(),
+        widget=forms.DateInput(attrs={'type': 'date'})
     )
     description = forms.CharField(
         max_length=100
     )
     
-    
+    def __init__(self, user, *args, **kwargs):
+        super(AddTransactionForm, self).__init__(*args, **kwargs)
+        self.fields['account'].choices = \
+            [(acc.id, acc.name) for acc in Account.objects.filter(user=user)]
+        self.fields['currency'].initial = user.settings.first().main_currency
+        
