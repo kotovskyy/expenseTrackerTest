746e92978fbc9b5b7e9041de4821b6b3e73582ec
acodered <alexkondratru@gmail.com>
2023-11-12 15:13:45 +0100

    Create EditTransactionForm

--- expensetracker/forms.py
+++ expensetracker/forms.py
@@ -27,6 +27,38 @@ class AddTransactionForm(forms.Form):
     def __init__(self, user, *args, **kwargs):
         super(AddTransactionForm, self).__init__(*args, **kwargs)
         self.fields['account'].choices = \
-            [(acc.id, acc.name) for acc in Account.objects.filter(user=user)]
+            [(acc.id, acc.name) for acc in user.accounts.filter(user=user)]
         self.fields['currency'].initial = user.settings.first().main_currency
+        
+        
+class EditTransactionForm(forms.Form):
+        
+    amount = forms.DecimalField(
+        label="Transaction Amount",
+        max_digits=22,
+        decimal_places=2
+    )
+    currency = forms.ChoiceField(
+        choices=[(c.id, c.code) for c in Currency.objects.all()],
+        initial=None
+    )
+    account = forms.ChoiceField(choices=[])
+    category = forms.ChoiceField(choices=[])
+    date = forms.DateField(
+        label="Date",
+        initial=date.today(),
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
+    description = forms.CharField(
+        max_length=100
+    )
+    
+    def __init__(self, user, *args, **kwargs):
+        super(EditTransactionForm, self).__init__(*args, **kwargs)
+        self.fields['account'].choices = \
+            [(acc.id, acc.name) for acc in user.accounts.filter(user=user)]
+        self.fields['category'].choices = \
+            [(c.id, c.name) for c in user.categories.filter(user=user)]
+        self.fields['currency'].initial = user.settings.first().main_currency
+        
         
