fc720c906b14fe164d3fed2e04d87e9fbb995a9e
acodered <alexkondratru@gmail.com>
2023-11-21 19:36:52 +0100

    Add urls for new views

--- expensetracker/urls.py
+++ expensetracker/urls.py
@@ -14,5 +14,11 @@ urlpatterns = [
     path('transactions/<int:transaction_id>/', views.transaction_edit_page,
          name='expensetracker_transaction-edit-page'),
     path('transactions/<int:transaction_id>/delete/', views.transaction_delete,
-         name='expensetracker_transaction-delete')
+         name='expensetracker_transaction-delete'),
+    path('accounts/<int:account_id>/', views.account_page,
+         name='expensetracker_account-page'),
+    path('accounts/<int:account_id>/addincome/', views.add_account_income,
+         name='expensetracker_add-account-income'),
+    path('homepage/addcategory/', views.add_new_category, 
+         name='expensetracker_add-new-category'),
 ]
